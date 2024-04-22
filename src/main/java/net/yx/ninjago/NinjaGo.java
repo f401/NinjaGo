c3cdc65657549d0843ff3d94a01e1674d3029b07
f401 <nanLianQing@outlook.com>
2024-04-23 01:03:04 +0800

    update

--- src/main/java/net/yx/ninjago/NinjaGo.java
+++ src/main/java/net/yx/ninjago/NinjaGo.java
@@ -2,7 +2,7 @@ package net.yx.ninjago;
 
 import net.minecraftforge.fml.common.Mod;
 
-@Mod.EventBusSubscriber(modid = NinjaGo.MOD_ID)
+@Mod(NinjaGo.MOD_ID)
 public class NinjaGo {
 	public static final String MOD_ID = "ninjago";
 	
